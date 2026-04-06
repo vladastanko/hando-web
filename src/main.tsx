d5347571339841cdce38f5b35696989f7c4690fa
Vladimir Stankovic <vladastankovic1995@gmail.com>
2026-04-06 11:46:36 +0200

    fix: remove .tsx extensions from imports for Vercel build

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-import { ErrorBoundary } from './components/ui/ErrorBoundary.tsx'
+import App from './App'
+import { ErrorBoundary } from './components/ui/ErrorBoundary'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
